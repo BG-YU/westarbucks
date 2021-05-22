b09aaf1ecc73f30c27e6f4c4212f41713baa8970
yu <yu@yuui-MacBookPro.local>
2021-05-22 18:02:32 +0900

    COMMIT : westarbucks model 완성

--- products/models.py
+++ products/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, DecimalField, IntegerField, TextField
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'menu'
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'categories'
+
+class Nutrition(models.Model):
+    one_serving_kcal = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    sodium_mg = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    saturated_fat_g = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    sugars_g = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    protein_g = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    caffeine_mg = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    size_ml = models.CharField(max_length=45, null=True)
+    size_fluid_ounce = models.CharField(max_length=45, null=True)
+
+    class Meta:
+        db_table = 'nutritions'
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'allergy'
+
+class Product(models.Model):
+    # Category가 삭제되면 같이 Delete?? 기존 Category는 삭제되지만 현재의 물품은 그냥 Category변경이라면?
+    # 일단 CASCADE 미적용
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    korean_name = models.CharField(max_length=45)
+    english_name = models.CharField(max_length=45)
+    description = models.TextField()
+
+    # 상품을 날린다고 영양정보가 날라가면 안되니 CASCADE미적용
+    # 다른 상품에서 해당 영양정보를 사용가능할것 같기는 하나, 영양정보가 완전 동일한 상품이 있을지는 미지수...
+    # 아무튼 일단 미적용
+    nutrition = models.ForeignKey(Nutrition, on_delete=models.DO_NOTHING)
+
+    # Many to Many 설정
+    # 중간테이블에 컬럼을 추가해서 생성을 할려면 through를 통해 중간테이블을 명시해주소
+    # clsss를 중간테이블명으로 헤서 수동으로 생성해주면 되는듯?
+    allegry = models.ManyToManyField(Allergy)
+
+    class Meta:
+        db_table = 'products'
+
+class Image(models.Model):
+    image_url = models.CharField(max_length=200)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'images'
